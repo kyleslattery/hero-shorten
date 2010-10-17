fa0019b349a4a65ada40dbb3899cb6541755878c
Kyle Slattery <k@kslat.com>
2010-10-17 18:03:23 -0400

    Add rudimentary admin panel

--- shortener.rb
+++ shortener.rb
@@ -37,6 +37,17 @@ get '/links/?' do
   erb :list
 end
 
+# Admin section
+get '/-/?' do
+  @links = Link.all(:order => [:id.desc])
+  erb :'links/index'
+end
+
+post '/-/create' do
+  @link = Link.first_or_create(:url => params[:url])
+  redirect "/-/"
+end
+
 get '/:slug/?' do
   if @link = Link.get_by_slug(params[:slug])
     response['Cache-Control'] = 'public, max-age=300'
