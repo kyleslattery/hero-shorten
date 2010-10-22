3b4c74bf6cd24bb14103de7f0f57667a8f0c7641
Kyle Slattery <k@kslat.com>
2010-10-22 00:33:23 -0400

    Add rudimentary authentication for admin panel using heroku env variables
    set ADMIN_USERNAME and ADMIN_PASSWORD to use

--- shortener.rb
+++ shortener.rb
@@ -6,6 +6,7 @@ require 'dm-core'
 require 'dm-postgres-adapter'
 require 'dm-migrations'
 require 'dm-validations'
+require 'encryptor'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'])
 
@@ -14,19 +15,52 @@ require 'lib/link'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-get '/' do  
+enable :sessions
+
+def logged_in?
+  return false if session['key'].nil?
+  
+  Encryptor.decrypt(:value => session['key'], :key => ENV['DATABASE_URL']) == request.ip
+end
+
+def require_log_in
+  redirect '/-/login' unless logged_in?
+end
+
+get '/' do
   redirect 'http://kyleslattery.com'
 end
 
 # Admin section
 get '/-/?' do
+  require_log_in
+  
   @links = Link.all(:order => [:id.desc])
   erb :'links/index'
 end
 
 post '/-/create' do
+  require_log_in
+  
   @link = Link.first_or_create(:url => params[:url])
   redirect "/-/"
+end
+
+get '/-/login' do
+  erb :login
+end
+
+get '/-/logout' do
+  session['key'] = nil
+  redirect "/-/login"
+end
+
+post '/-/login' do
+  redirect '/-/login' if params[:password] != ENV['ADMIN_PASSWORD'] ||
+                         params[:username] != ENV['ADMIN_USERNAME']
+                         
+  session['key'] = Encryptor.encrypt(:value => request.ip, :key => ENV['DATABASE_URL'])
+  redirect '/-/'
 end
 
 get '/:slug/?' do
